1fd0313b3493338c6d196d53386f43e4140c11fb
James Stanley <james@incoherency.co.uk>
2019-09-29 16:21:26 +0100

    Always make bottom layer 400px in alrgest dimension

--- lostplatoon/js/ui.js
+++ lostplatoon/js/ui.js
@@ -5,9 +5,14 @@ $('#form').submit(function(e) {
     r.readAsBinaryString($('#stlfile')[0].files[0]);
     $(r).on('load', function(e) {
         let model = load_stl(e.target.result);
-        console.log(process_model(model, $('#material').val()));
+        let proc = process_model(model, $('#material').val());
+        console.log(proc);
 
-        $('#output')[0].appendChild(draw_bottom_layer(model, 0.2, 10));
+        let maxdim = 400; // px
+        let px_per_mm_x = maxdim / proc.bounding_box.size.x;
+        let px_per_mm_y = maxdim / proc.bounding_box.size.y;
+
+        $('#output')[0].appendChild(draw_bottom_layer(model, 0.2, px_per_mm_x < px_per_mm_y ? px_per_mm_x : px_per_mm_y));
 
         // TODO: generate a crude bitmap (maybe 1mm per pixel?) of the bottom layer, display it to the user, generate initial sprue points, and let them add/remove sprue points
 
