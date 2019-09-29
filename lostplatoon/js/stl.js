160025e278ff4ef8c7b9e2cdcdfa172c0e68a832
James Stanley <james@incoherency.co.uk>
2019-09-29 16:19:21 +0100

    Support ASCII STL

--- lostplatoon/js/stl.js
+++ lostplatoon/js/stl.js
@@ -1,5 +1,6 @@
 // read an STL file from the input and return a list of triangles of the form
 //   [ [{x:..., y:..., z:...}, {x:..., y:..., z:...}, {x:..., y:..., z:...}], ... ]
+// this function passes over to load_ascii_stl() if the file doesn't look like a valid binary STL
 function load_stl(data) {
     let ptr = 0;
 
@@ -26,9 +27,10 @@ function load_stl(data) {
     // uint32 number of triangles
     let ntris = readuint32();
 
-    // check the file length is correct
-    if (data.length != 84 + ntris*(12 + 3*12 + 2))
-        throw "Invalid stl file (incorrect length)";
+    if (data.length != 84 + ntris*(12 + 3*12 + 2)) {
+        // if the file length is incorrect, try loading it as an ASCII STL
+        return load_ascii_stl(data);
+    }
 
     let model = [];
     for (let i = 0; i < ntris; i++) {
@@ -52,6 +54,51 @@ function load_stl(data) {
         // uint16 attribute byte count
         skip(2);
     }
+
+    return model;
+}
+
+// read an ASCII STL file from the input and return a list of triangles of the form
+//   [ [{x:..., y:..., z:...}, {x:..., y:..., z:...}, {x:..., y:..., z:...}], ... ]
+// you should normally call load_stl() instead of load_ascii_stl() -- that function calls
+// this function if the data doesn't look like a valid binary STL
+function load_ascii_stl(data) {
+    parts = data.split(/\s+/);
+
+    let ptr = 0;
+
+    let expect = function(str) {
+        if (parts[ptr++] != str)
+            throw "Invalid STL file (expected " + str + ")";
+    }
+
+    expect('solid');
+    ptr++; // skip solid name
+
+    let model = [];
+
+    while (ptr < parts.length - 3) {
+        expect('facet'); expect('normal');
+        ptr += 3; // skip normal vector
+        expect('outer'); expect('loop');
+
+        // 3 vertices
+        let triangle = [];
+        for (let i = 0; i < 3; i++) {
+            let vertex = {};
+            expect('vertex');
+            vertex.x = parseFloat(parts[ptr++]);
+            vertex.y = parseFloat(parts[ptr++]);
+            vertex.z = parseFloat(parts[ptr++]);
+            triangle.push(vertex);
+        }
+        model.push(triangle);
+
+        expect('endloop');
+        expect('endfacet');
+    }
+
+    expect('endsolid');
 
     return model;
 }
